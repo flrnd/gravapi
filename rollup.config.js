9cbfe10a99e2a3a6139108629251f66ef6c095e2
Flrn Prz <rauperez@gmail.com>
2019-09-18 20:35:30 +0200

    minor package tweaks and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: './src/cli.js',
   output: {
-    file: './build/app.js',
+    file: './build/cli.js',
     format: 'cjs',
     name: 'gravapi-cli',
   },
@@ -11,5 +13,9 @@ export default {
     json({
       exclude: ['node_modules'],
     }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    terser(),
   ],
 };
